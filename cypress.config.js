71c2a8b560ba7db8c02e055dd3e5c5d7e7c3fc97
ilyas <ilyas.soufiane96@gmail.com>
2024-04-09 08:20:34 +0200

    edit attribute projectId from buaqfo to iy8pni

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-	projectId: "buaqfo",
+	projectId: "iy8pni",
   e2e: {
     // Configure your E2E tests here
     specPattern: "cypress/e2e/Specs/*.{cy,spec}.{js,ts}"
